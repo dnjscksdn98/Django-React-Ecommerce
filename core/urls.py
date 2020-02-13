c3f249a12e77c4cd58e519763ee0167704e6f3a5
Alex <dnjscksdn98@gmail.com>
2020-02-13 14:12:14 +0900

    Update initial settings

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+
     path('', include('ecommerce.urls')),
+    path('api/', include('ecommerce.api.urls')),
 ]
 
 if settings.DEBUG:
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -5,7 +5,7 @@ from django_countries.fields import CountryField
 
 
 CATEGORY_CHOICES = (
-    ('T', 'TV'),
+    ('T', 'Tablet'),
     ('P', 'Phone'),
     ('L', 'Laptop')
 )
