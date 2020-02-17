fcf92501037c4bdf62bc0268c6aa6efa9f6861e4
Alex <dnjscksdn98@gmail.com>
2020-02-17 18:22:08 +0900

    Update : handling address update & delete

--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -213,11 +213,63 @@ class AddressCreateView(CreateAPIView):
     serializer_class = AddressSerializer
     queryset = Address.objects.all()
 
+    def post(self, request, *args, **kwargs):
+        form = request.data.get('formData', None)
+        if form is None:
+            return Response({"message": "Invalid data received"}, status=HTTP_400_BAD_REQUEST)
+
+        address_type = request.data.get('address_type')
+        address = Address(
+            user=request.user,
+            street_address=form['street_address'],
+            apartment_address=form['apartment_address'],
+            country=form['country'],
+            zip=form['zip'],
+            address_type=address_type,
+            default=form['default']
+        )
+
+        current_default_address = Address.objects.filter(
+            user=request.user, default=True, address_type=address_type)
+        if current_default_address.exists() and address.default == True:
+            current_default_address = current_default_address.first()
+            current_default_address.default = False
+            current_default_address.save()
+
+        address.save()
+        return Response(status=HTTP_200_OK)
+
 
 class AddressUpdateView(UpdateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AddressSerializer
     queryset = Address.objects.all()
+
+    def put(self, request, *args, **kwargs):
+        form = request.data.get('formData', None)
+        if form is None:
+            return Response({'message': 'Invalid data received'}, status=HTTP_400_BAD_REQUEST)
+
+        id = form['id']
+        default = form['default']
+        address_type = request.data.get('address_type')
+        address = Address.objects.get(
+            id=id, user=request.user, address_type=address_type)
+        current_default_address = Address.objects.filter(
+            user=request.user, default=True, address_type=address_type)
+
+        if current_default_address.exists() and default == True:
+            current_default_address = current_default_address.first()
+            current_default_address.default = False
+            current_default_address.save()
+
+        address.street_address = form['street_address']
+        address.apartment_address = form['apartment_address']
+        address.country = form['country']
+        address.zip = form['zip']
+        address.default = default
+        address.save()
+        return Response(status=HTTP_200_OK)
 
 
 class AddressDeleteView(DestroyAPIView):
